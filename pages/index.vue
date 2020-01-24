cb48143d537a4d235c9e9ac87778bdff3f3b5735
Attanza <myrbx.747@gmail.com>
2020-01-24 21:13:06 +0700

    add dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,81 @@
 <template>
-  <div>default page</div>
+  <div>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 md4>
+        <v-card v-if="dashboardData" light class="elevation-2 mb-2" max-height="200px">
+          <v-layout>
+            <v-flex xs4 class="flex-all-centered">
+              <v-icon color="primary" style="font-size: 3rem;">supervised_user_circle</v-icon>
+            </v-flex>
+            <v-flex xs8>
+              <div>Active Marketings</div>
+              <div class="title primary--text">{{ dashboardData.total_marketings }}</div>
+            </v-flex>
+          </v-layout>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 sm6 md4>
+        <v-card v-if="dashboardData" light class="elevation-2 mb-2" max-height="200px">
+          <v-layout>
+            <v-flex xs4 class="flex-all-centered">
+              <v-icon color="primary" style="font-size: 3rem;">account_balance</v-icon>
+            </v-flex>
+            <v-flex xs8>
+              <div>Registered Universities</div>
+              <div class="title primary--text">{{ dashboardData.total_universities }}</div>
+            </v-flex>
+          </v-layout>
+        </v-card>
+      </v-flex>
+      <!-- <v-flex xs12 sm6 md4>
+        <v-card v-if="dashboardData" light class="elevation-8 mb-2" max-height="200px">
+          <v-layout>
+            <v-flex xs4 class="flex-all-centered">
+              <v-icon color="primary" style="font-size: 4rem;">donut_small</v-icon>
+            </v-flex>
+            <v-flex xs8>
+              <v-card-title primary-title>
+                <div>
+                  <div>Products</div>
+                  <div class="headline primary--text"> {{ dashboardData.total_products }}</div>
+                </div>
+              </v-card-title>
+            </v-flex>
+          </v-layout>
+        </v-card>
+      </v-flex>-->
+    </v-layout>
+  </div>
 </template>
-
 <script>
-export default {};
-</script>
+import { DASHBOARD_DATA_URL } from "~/utils/apis";
+import catchError from "~/utils/catchError";
+import { global } from "~/mixins";
 
-<style lang="scss" scoped>
+export default {
+  mixins: [global],
+  mounted() {
+    this.seedData();
+  },
+  methods: {
+    async seedData() {
+      try {
+        this.activateLoader();
+        let resp = await this.$axios.$get(DASHBOARD_DATA_URL);
+        if (resp) this.$store.commit("dashboardData", resp);
+        this.deactivateLoader();
+      } catch (e) {
+        this.deactivateLoader();
+        catchError(e);
+      }
+    }
+  }
+};
+</script>
+<style scoped>
+.flex-all-centered {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
