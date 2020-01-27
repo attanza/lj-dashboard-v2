2acb5ecb7cf6b9f01fcae614ec4db0aace6359c8
Attanza <myrbx.747@gmail.com>
2020-01-27 15:22:14 +0700

    add marketing actions

---
+++ pages/marketing-actions/_id.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2 v-if="currentEdit" class="headline primary--text mb-2">
+      {{ currentEdit.name }}
+    </h2>
+    <v-tabs align-with-title color="primary" class="white elevation-1">
+      <v-tabs-slider color="white" />
+      <v-tab href="#detail">Detail</v-tab>
+      <v-tab-item :id="'detail'">
+        <detail />
+      </v-tab-item>
+    </v-tabs>
+  </div>
+</template>
+
+<script>
+import { MARKETING_ACTION_URL } from "~/utils/apis";
+import axios from "axios";
+import { detail, dform } from "~/components/marketing-actions";
+import { global } from "~/mixins";
+import catchError from "~/utils/catchError";
+
+export default {
+  async fetch({ store, params, redirect, $axios, $router, $auth }) {
+    try {
+      let resp = await $axios.$get(MARKETING_ACTION_URL + "/" + params.id);
+
+      store.commit("currentEdit", resp.data);
+    } catch (e) {
+      if (process.client) catchError(e, $router, $auth);
+      else {
+        redirect("/");
+      }
+    }
+  },
+  components: { detail, dform },
+  mixins: [global]
+};
+</script>
+
+<style scoped></style>
---
+++ pages/marketing-actions/index.vue
@@ -0,0 +1,131 @@
+<template>
+  <v-card>
+    <v-card-title class="primary--text">{{ title }}</v-card-title>
+    <v-toolbar flat color="transparent">
+      <Tbtn
+        v-if="checkPermission('create-marketing-action')"
+        :bottom="true"
+        :tooltip-text="'Tambahkan ' + title"
+        icon-mode
+        icon="add"
+        color="primary"
+        @onClick="showForm = !showForm"
+      />
+      <Tbtn
+        :bottom="true"
+        :tooltip-text="'Download data ' + title"
+        icon-mode
+        icon="cloud_download"
+        color="primary"
+        @onClick="downloadData"
+      />
+
+      <v-spacer />
+      <v-text-field
+        v-model="options.search"
+        append-icon="search"
+        label="Cari"
+        single-line
+        hide-details
+      />
+    </v-toolbar>
+    <v-card-text class="mt-4">
+      <v-data-table
+        :headers="headers"
+        :items="items"
+        :search="options.search"
+        :loading="loading"
+        :options.sync="options"
+        :footer-props="footerProps"
+        :server-items-length="total"
+      >
+        <template v-slot:item.name="{ item }">
+          <v-btn text color="primary" nuxt :to="`${link}/${item.id}`">
+            {{ item.name }}
+          </v-btn>
+        </template>
+      </v-data-table>
+    </v-card-text>
+    <dform
+      :show="showForm"
+      @onClose="showForm = false"
+      @onAdd="addData"
+      :link="link"
+    />
+    <DownloadDialog
+      :show-dialog="showDownloadDialog"
+      :data-to-export="dataToExport"
+      :fillable="fillable"
+      :type-dates="typeDates"
+      model="MarketingAction"
+      @onClose="showDownloadDialog = false"
+    />
+  </v-card>
+</template>
+
+<script>
+import debounce from "lodash/debounce";
+import { headers, downloadData } from "~/components/marketing-actions/util";
+import { global, catchError } from "~/mixins";
+import { dform } from "~/components/marketing-actions";
+import DownloadDialog from "~/components/DownloadDialog";
+export default {
+  mixins: [global, catchError],
+  components: { DownloadDialog, dform },
+  data() {
+    return {
+      title: "Marketing Action",
+      link: "/marketing-actions",
+      headers: headers,
+      fillable: downloadData,
+      typeDates: ["created_at"],
+      dataToExport: []
+    };
+  },
+  mounted() {
+    this.populateTable();
+  },
+  watch: {
+    options: {
+      handler: debounce(function() {
+        if (!this.loading) {
+          this.populateTable();
+        }
+      }, 500),
+      deep: true
+    }
+  },
+  methods: {
+    async populateTable() {
+      try {
+        this.activateLoader();
+        const queries = this.getQueries();
+        const resp = await this.$axios.$get(`${this.link + queries}`);
+        this.total = resp.meta.total;
+        this.items = resp.data;
+        this.deactivateLoader();
+      } catch (e) {
+        this.deactivateLoader();
+        this.showForm = false;
+        this.catchError(e, null, this.$router);
+      }
+    },
+    toDetail(data) {
+      this.$router.push(`${this.link}/${data.id}`);
+    },
+    addData(data) {
+      this.items.unshift(data);
+      this.showForm = false;
+    },
+    downloadData() {
+      this.dataToExport = [];
+      this.dataToExport = this.items;
+      if (this.dataToExport.length) {
+        this.showDownloadDialog = true;
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
