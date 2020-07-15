6d9ad0c15f33ed85bdeab7ba9d2c206201884831
Dani Dan <dani@getmika.id>
2020-07-15 12:25:10 +0700

    refactor ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
   apps: [
     {
       name: "dashboard",
-      script: "./node_modules/nuxt/bin/nuxt-start",
+      script: "./node_modules/nuxt/bin/nuxt.js",
+      args: "start",
       env: {
         HOST: "0.0.0.0",
         PORT: 13000,
